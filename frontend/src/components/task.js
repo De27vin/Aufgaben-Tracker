d91335985f8ffd9aac5c305e8dc5581421b584e1
Devin de Spindler <devin.vipxxx@gmail.com>
2024-12-01 15:02:59 +0100

    fix: Date format

--- frontend/src/components/task.js
+++ frontend/src/components/task.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 const Task = ({ task, deleteTask }) => {
+
+  const formatedDate = (dateFormat) => {
+    const date = new Date(dateFormat);
+    return date.toLocaleDateString("de-DE");
+  }
+
   return (
     <div className="task">
       <h3>{task.title}</h3>
       <p>{task.description}</p>
       <p>Priority: {task.priority}</p>
-      <p>Due to: {task.dueDate}</p>
+      <p>Due to: {formatedDate(task.dueDate)}</p>
       <button onClick={() => deleteTask(task._id)}>Delete</button>
     </div>
   );
